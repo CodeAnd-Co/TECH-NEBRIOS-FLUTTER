f018a25a494db2cf255cae47b133704a509f1bb8
Emiliano <a01710711@tec.mx>
2025-04-23 06:50:02 -0600

    Agrega cambio en el repository

--- tech_nebrios_tracker/lib/data/repositories/charola_repository.dart
+++ tech_nebrios_tracker/lib/data/repositories/charola_repository.dart
@@ -1,11 +1,30 @@
 import '../models/charola_model.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class CharolaRepository {
-  final List<CharolaModel> _charolas = []; // Simulación de almacenamiento
+  final List<CharolaModel> _charolas = []; // Simulacion de almacenamiento
 
-  // Método para guardar una charola
+  // Metodo para guardar una charola
   void saveCharola(CharolaModel charola) {
     _charolas.add(charola);
     print("Charola guardada: ${charola.nombre}");
+  }
+
+  // Metodo para obtener datos de alimentacion desde el backend
+  Future<List<String>> getAlimentos() async {
+    final url = Uri.parse(
+      'https://localhost:3000/', // Cambia por tu endpoint
+    ); // Cambia por tu endpoint
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      // Decodifica la respuesta JSON
+      final List<dynamic> data = json.decode(response.body);
+      // Convierte los datos en una lista de strings
+      return data.map((item) => item['nombre'].toString()).toList();
+    } else {
+      throw Exception('Error al obtener los alimentos: ${response.statusCode}');
+    }
   }
 }

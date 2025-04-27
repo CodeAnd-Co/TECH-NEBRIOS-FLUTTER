bfa52d0a63dbd1e49970bcba3e4c57c7683a2c9e
El3Du4Rd0 <a01710168@tec.mx>
2025-04-27 15:59:13 -0600

    eliminación de Popup obsoleto y modificación de main anterior

--- tech_nebrios_tracker/lib/main.dart
+++ tech_nebrios_tracker/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './framework/views/components/pop_up.dart';
-
 void main() {
   runApp(const MainApp());
 }
@@ -12,30 +10,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(), // Usa un widget separado
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Popup.show(
-              context: context,
-              title: '¡Hola!',
-              content: 'Este es un popup funcional',
-              onConfirm: () {
-                print('Confirmado');
-              },
-            );
-          },
-          child: const Text('Mostrar Popup'),
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
         ),
       ),
     );

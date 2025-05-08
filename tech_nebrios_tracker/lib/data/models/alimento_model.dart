a755bad910b8d851f4dc2cd7c9cbf42cdc456a54
Mariana Juarez <maris_bonjour@hotmail.com>
2025-05-08 13:46:00 -0600

    Documentar

--- tech_nebrios_tracker/lib/data/models/alimento_model.dart
+++ tech_nebrios_tracker/lib/data/models/alimento_model.dart
@@ -1,15 +1,26 @@
 //RF23: Registrar un nuevo tipo de comida en el sistema - https://codeandco-wiki.netlify.app/docs/proyectos/larvas/documentacion/requisitos/RF23
+
+/// Modelo que representa un alimento.
+///
+/// Contiene identificador, nombre y descripción del alimento.
 class Alimento {
+  /// ID del alimento.
   final int idAlimento;
+
+  /// Nombre del alimento.
   final String nombreAlimento;
+
+  /// Descripción del alimento.
   final String descripcionAlimento;
 
+  /// Constructor para crear una instancia de [Alimento].
   Alimento({
     required this.idAlimento,
     required this.nombreAlimento,
     required this.descripcionAlimento,
   });
-
+  
+  /// Crea un objeto [Alimento] desde un mapa JSON.
   factory Alimento.fromJson(Map<String, dynamic> json) {
     return Alimento(
       idAlimento: json['comidaId'] as int,

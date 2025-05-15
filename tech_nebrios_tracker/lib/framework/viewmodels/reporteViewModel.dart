75abe5d40ea41c6bf146ebabd2abc3e368c299ff
Armando Méndez Castro <armandomendezcastro@MacBook-Pro-de-Armando-3.local>
2025-05-15 01:34:00 -0600

    Merge local con develop

--- tech_nebrios_tracker/lib/framework/viewmodels/reporteViewModel.dart
+++ tech_nebrios_tracker/lib/framework/viewmodels/reporteViewModel.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../../domain/reporteUseCases.dart';
-import '../../data/repositories/reporteRepository.dart';
 import 'package:open_file/open_file.dart';
 
 
